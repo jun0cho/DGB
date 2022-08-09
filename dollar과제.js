918ed93ce308064017abf78bce622ae95e57b063
jun0cho <100102375+jun0cho@users.noreply.github.com>
2022-08-09 14:07:20 +0900

    깃설정연습

---
+++ dollar과제.js
@@ -0,0 +1,9 @@
+a = '2022.12.01';
+
+b = a.split('.');
+
+console.log(b);
+
+if(a.split('.')[1] == '12'){
+    console.log('hello!!');
+}
---
+++ throw_error.js
@@ -0,0 +1,15 @@
+// catch 로 연결됩니다.
+const throwError = new Promise((resolve, reject) => {
+    throw Error("error");
+  });
+  throwError
+    .then(() => console.log("throwError success"))
+    .catch(() => console.log("throwError catched"));
+
+const several2 = new Promise((resolve, reject) => {
+    reject();
+    resolve();
+  });
+  several2
+    .then(() => console.log("several2 success"))
+    .catch(() => console.log("several2 catched"));
